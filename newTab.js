60f4246530912cd3e1ef22b74bc9229eb49876ab
Nick_edu <nick_edu@nameless.T-mobile.com>
2018-02-22 15:49:56 -0800

    Cleans up newTab.js

--- newTab.js
+++ newTab.js
@@ -1,24 +1,35 @@
 
 
 window.onload = function() {
-//   var startPos;
-//   var geoSuccess = function(position) {
-//     startPos = position;
-//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-//   };
-//   navigator.geolocation.getCurrentPosition(geoSuccess);
+  var startPos;
+  var weatherData;
+  async function geoSuccess(position) {
+    startPos = position;
+    // console.log(startPos);
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+
+    getWeather(startPos.coords.latitude,startPos.coords.longitude);
+  };
+  
+  function printData(data){
+    console.log(data)
+  }
 
 
-
+function getWeather(lat, lon){
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8a8b7899a71e133a0af9805244c61e66",
+    url: "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=8a8b7899a71e133a0af9805244c61e66",
   }).done(function(data){
-    console.log(data);
-  })
+    printData(data);
+  });
+}
 
+// Get the user location
 
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+  
 };
 
 
