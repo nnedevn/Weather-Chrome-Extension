e444f326969ad224e8a4ddc30460dc4125e98a56
Nick_edu <nick_edu@nameless.local>
2018-02-22 13:27:42 -0800

    Adds weather api

--- newTab.js
+++ newTab.js
@@ -1,5 +1,24 @@
-console.log($);
 
-function getWeather(){
 
-}
+window.onload = function() {
+//   var startPos;
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   navigator.geolocation.getCurrentPosition(geoSuccess);
+
+
+
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8a8b7899a71e133a0af9805244c61e66",
+  }).done(function(data){
+    console.log(data);
+  })
+
+
+};
+
+
