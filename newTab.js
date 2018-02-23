53902202083d953a21b202a38641c9a68c3221e0
Nick Nedev <nedevn@gmail.com>
2018-02-22 17:50:20 -0800

    Adds dynamically generated static gogole-map image

--- newTab.js
+++ newTab.js
@@ -1,35 +1,31 @@
-
-
 window.onload = function() {
+  
   var startPos;
   var weatherData;
   async function geoSuccess(position) {
     startPos = position;
     // console.log(startPos);
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    // document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    // document.getElementById('startLon').innerHTML = startPos.coords.longitude;
 
     getWeather(startPos.coords.latitude,startPos.coords.longitude);
   };
-  
-  function printData(data){
-    console.log(data)
-  }
-
 
 function getWeather(lat, lon){
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=8a8b7899a71e133a0af9805244c61e66",
+    url: "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=8a8b7899a71e133a0af9805244c61e66",
   }).done(function(data){
     printData(data);
   });
 }
 
+function printData(data){
+  $('#city').text("Current Temperature in " + data.name);
+  $('#temp').text(data.main.temp +"C");
+  console.log(data)
+}
 // Get the user location
-
   navigator.geolocation.getCurrentPosition(geoSuccess);
   
 };
-
-
